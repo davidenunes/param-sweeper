dbeaf6f306209f6d07270f481b2419e94a2e0883
Davide Nunes <davex.pt@gmail.com>
2013-03-14 18:36:52 +0000

    run parameter added to combined parameter sweep

--- src/main/java/org/bhave/sweeper/impl/DefaultCombinedParameterSweep.java
+++ src/main/java/org/bhave/sweeper/impl/DefaultCombinedParameterSweep.java
@@ -32,166 +32,193 @@ import org.bhave.sweeper.CombinedParameterSweep;
 import org.bhave.sweeper.ParameterSweep;
 
 /**
- * <p> This class allows the creation of {@link CombinedParameterSweep} objects
+ * <p>
+ * This class allows the creation of {@link CombinedParameterSweep} objects
  * which can be used to combine multiple {@link ParameterSweep} objects into one
  * structure that implements {@link Iterable} of {@link Configuration} objects.
- * </p> <p> The iterator for this class generates {@link Configuration} objects
- * with all the possible parameter combinations according to the parameter
- * sweeps used to create this object. </p> <p> The order of the generated
- * configurations also depends on the order of the parameter sweeps passed down
- * in the list. </p> <p> Example:<br> <br> for two parameters <b>p1 = {0,1} and
- * p2 = {2,3}</b>, the combined parameter sweep will provide an iterator that
- * generates the configurations:<br> <br> c1 = {p1=0, p2=2}<br> c2 = {p1=0,
- * p2=3}<br> c3 = {p1=1, p2=2}<br> c4 = {p1=1, p2=3}<br> <br>
- *
+ * </p>
+ * <p>
+ * The iterator for this class generates {@link Configuration} objects with all
+ * the possible parameter combinations according to the parameter sweeps used to
+ * create this object.
+ * </p>
+ * <p>
+ * The order of the generated configurations also depends on the order of the
+ * parameter sweeps passed down in the list.
+ * </p>
+ * <p>
+ * Example:<br>
+ * <br>
+ * for two parameters <b>p1 = {0,1} and p2 = {2,3}</b>, the combined parameter
+ * sweep will provide an iterator that generates the configurations:<br>
+ * <br>
+ * c1 = {p1=0, p2=2}<br>
+ * c2 = {p1=0, p2=3}<br>
+ * c3 = {p1=1, p2=2}<br>
+ * c4 = {p1=1, p2=3}<br>
+ * <br>
+ * 
  * also, the number of runs passed will repeat each configuration a number of
- * times equals to the given runs value.<br> <br>
- *
- * For example, for a number of runs = 2, the configuration values would be:
- * <br> <br> c1 = {p1=0, p2=2}<br> c2 = {p1=0, p2=2}<br> c3 = {p1=0, p2=3}<br>
- * c4 = {p1=0, p2=3}<br> c5 = {p1=1, p2=2}<br> c6 = {p1=1, p2=2}<br> c7 = {p1=1,
- * p2=3}<br> c8 = {p1=1, p2=3}<br>
- *
+ * times equals to the given runs value.<br>
+ * <br>
+ * 
+ * For example, for a number of runs = 2, the configuration values would be: <br>
+ * <br>
+ * c1 = {p1=0, p2=2}<br>
+ * c2 = {p1=0, p2=2}<br>
+ * c3 = {p1=0, p2=3}<br>
+ * c4 = {p1=0, p2=3}<br>
+ * c5 = {p1=1, p2=2}<br>
+ * c6 = {p1=1, p2=2}<br>
+ * c7 = {p1=1, p2=3}<br>
+ * c8 = {p1=1, p2=3}<br>
+ * 
  * </p>
  */
 public class DefaultCombinedParameterSweep implements CombinedParameterSweep {
 
-    private List<ParameterSweep> params;
-    private int runs;
+	private List<ParameterSweep> params;
+	private int runs;
 
-    public DefaultCombinedParameterSweep(List<ParameterSweep> params, int numRuns) {
-        this.params = new ArrayList<>(params);
-        this.runs = numRuns;
-    }
+	public DefaultCombinedParameterSweep(List<ParameterSweep> params,
+			int numRuns) {
+		this.params = new ArrayList<>(params);
+		this.runs = numRuns;
+	}
 
-    @Override
-    public Iterator<Configuration> iterator() {
-        return new CombinedIterator();
-    }
+	@Override
+	public Iterator<Configuration> iterator() {
+		return new CombinedIterator();
+	}
 
-    @Override
-    public void setNumRuns(int numRuns) {
-        this.runs = numRuns;
-    }
+	@Override
+	public void setNumRuns(int numRuns) {
+		this.runs = numRuns;
+	}
 
-    @Override
-    public int getNumRuns() {
-        return runs;
-    }
+	@Override
+	public int getNumRuns() {
+		return runs;
+	}
 
-    /**
-     * An iterator that combines the existing parameter sweeps and provides a
-     * new {@link Configuration} iterator. Each Configuration object has all the
-     * parameters for the parameter sweeps contained in the Combined Parameter
-     * Sweep.
-     */
-    private class CombinedIterator implements Iterator<Configuration> {
+	/**
+	 * An iterator that combines the existing parameter sweeps and provides a
+	 * new {@link Configuration} iterator. Each Configuration object has all the
+	 * parameters for the parameter sweeps contained in the Combined Parameter
+	 * Sweep.
+	 */
+	private class CombinedIterator implements Iterator<Configuration> {
 
-        List<Iterator<Configuration>> iterators;
-        // index for the current iterator that needs a next
-        int currentIt;
-        Configuration config;
-        int numConfigs;
-        int currentNumRuns;
+		List<Iterator<Configuration>> iterators;
+		// index for the current iterator that needs a next
+		int currentIt;
+		Configuration config;
+		int numConfigs;
+		int currentNumRuns;
 
-        public CombinedIterator() {
-            iterators = new ArrayList<>(params.size());
-            // add iterators
-            for (ParameterSweep param : params) {
-                iterators.add(param.iterator());
-            }
-            currentIt = 0;
-            config = new PropertiesConfiguration();
-            numConfigs = 0;
-            currentNumRuns = 0;
-        }
+		public CombinedIterator() {
+			iterators = new ArrayList<>(params.size());
+			// add iterators
+			for (ParameterSweep param : params) {
+				iterators.add(param.iterator());
+			}
+			currentIt = 0;
+			config = new PropertiesConfiguration();
+			numConfigs = 0;
+			currentNumRuns = 0;
+		}
 
-        /**
-         * <p>Has more values if the Configuration objects generator did not
-         * reach the total size of this parameter sweep.</p>
-         *
-         * <p>Note that the size of a parameter sweep is the number of possible
-         * values it provides, in this case, all the possible parameter value
-         * combinations.</p>
-         *
-         * return true if it can generate more configurations
-         */
-        @Override
-        public boolean hasNext() {
-            return numConfigs < size();
-        }
+		/**
+		 * <p>
+		 * Has more values if the Configuration objects generator did not reach
+		 * the total size of this parameter sweep.
+		 * </p>
+		 * 
+		 * <p>
+		 * Note that the size of a parameter sweep is the number of possible
+		 * values it provides, in this case, all the possible parameter value
+		 * combinations.
+		 * </p>
+		 * 
+		 * return true if it can generate more configurations
+		 */
+		@Override
+		public boolean hasNext() {
+			return numConfigs < size();
+		}
 
-        /**
-         * Private recursive method that generates the configuration values it
-         * jumps from Iterator to iterator to supply the possible configuration
-         * instances.
-         */
-        private void createConfiguration() {
+		/**
+		 * Private recursive method that generates the configuration values it
+		 * jumps from Iterator to iterator to supply the possible configuration
+		 * instances.
+		 */
+		private void createConfiguration() {
 
-            if (hasNext()) {
-                if (iterators.get(currentIt).hasNext()) {
-                    iteratorToConfiguration();
-                    if (currentIt + 1 < params.size()) {
-                        currentIt++;
-                        createConfiguration();
-                    }
-                } else {
-                    // reset the current iterator and step back to the previous
-                    iterators.set(currentIt, params.get(currentIt).iterator());
-                    currentIt--;
-                    createConfiguration();
-                }
-            }
-        }
+			if (hasNext()) {
+				if (iterators.get(currentIt).hasNext()) {
+					iteratorToConfiguration();
+					if (currentIt + 1 < params.size()) {
+						currentIt++;
+						createConfiguration();
+					}
+				} else {
+					// reset the current iterator and step back to the previous
+					iterators.set(currentIt, params.get(currentIt).iterator());
+					currentIt--;
+					createConfiguration();
+				}
+			}
+		}
 
-        /**
-         * Utility method that extracts the value for the currentIterator and
-         * updates this value for the current configuration instance.
-         */
-        private void iteratorToConfiguration() {
-            String param = params.get(currentIt).getParameterName();
-            Object value = iterators.get(currentIt).next().getProperty(param);
-            config.setProperty(param, value);
-        }
+		/**
+		 * Utility method that extracts the value for the currentIterator and
+		 * updates this value for the current configuration instance.
+		 */
+		private void iteratorToConfiguration() {
+			String param = params.get(currentIt).getParameterName();
+			Object value = iterators.get(currentIt).next().getProperty(param);
+			config.setProperty(param, value);
+		}
 
-        /**
-         * Returns the next {@link Configuration} object
-         *
-         * @return a new configuration
-         */
-        @Override
-        public Configuration next() {
-            if (currentNumRuns == 0) {
-                createConfiguration();
+		/**
+		 * Returns the next {@link Configuration} object
+		 * 
+		 * @return a new configuration
+		 */
+		@Override
+		public Configuration next() {
+			if (currentNumRuns == 0) {
+				createConfiguration();
 
-            }
-            currentNumRuns = (currentNumRuns + 1) % runs;
-            numConfigs++;
+			}
+			// create a new configuration copy
+			PropertiesConfiguration result = new PropertiesConfiguration();
+			result.copy(config);
+			result.addProperty(RUN_PARAM, (currentNumRuns + 1));
 
-            //create a new configuration copy
-            PropertiesConfiguration result = new PropertiesConfiguration();
-            result.copy(config);
+			currentNumRuns = (currentNumRuns + 1) % runs;
+			numConfigs++;
 
-            return result;
-        }
+			return result;
+		}
 
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException(
-                    "Can't remove values from a parameter sweep iterator");
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException(
+					"Can't remove values from a parameter sweep iterator");
 
-        }
-    }
+		}
+	}
 
-    @Override
-    public int size() {
-        if (params.size() > 0) {
-            int size = 1;
-            for (ParameterSweep param : params) {
-                size *= param.size();
-            }
-            return size * runs;
-        }
-        return 0;
-    }
+	@Override
+	public int size() {
+		if (params.size() > 0) {
+			int size = 1;
+			for (ParameterSweep param : params) {
+				size *= param.size();
+			}
+			return size * runs;
+		}
+		return 0;
+	}
 }
--- src/test/java/org/bhave/sweeper/CombinedParameterSweepTest.java
+++ src/test/java/org/bhave/sweeper/CombinedParameterSweepTest.java
@@ -33,58 +33,59 @@ import org.junit.Test;
 
 public class CombinedParameterSweepTest {
 
-    @Test
-    public void test() {
-        IntegerSequenceSweep sweep1 = new IntegerSequenceSweep("p1", 0, -1, -1);
-        assertEquals(2, sweep1.size());
+	@Test
+	public void test() {
+		IntegerSequenceSweep sweep1 = new IntegerSequenceSweep("p1", 0, -1, -1);
+		assertEquals(2, sweep1.size());
 
-        IntegerSequenceSweep sweep2 = new IntegerSequenceSweep("p2", 0, 2, 1);
-        assertEquals(3, sweep2.size());
+		IntegerSequenceSweep sweep2 = new IntegerSequenceSweep("p2", 0, 2, 1);
+		assertEquals(3, sweep2.size());
 
-        IntegerSequenceSweep sweep3 = new IntegerSequenceSweep("p3", 0, 2, 1);
-        assertEquals(3, sweep3.size());
+		IntegerSequenceSweep sweep3 = new IntegerSequenceSweep("p3", 0, 2, 1);
+		assertEquals(3, sweep3.size());
 
-        SingleValueSweep<Integer> sweep4 = new SingleValueSweep<>("p4", 1);
+		SingleValueSweep<Integer> sweep4 = new SingleValueSweep<>("p4", 1);
 
-        List<ParameterSweep> params = new LinkedList<>();
-        params.add(sweep1);
-        params.add(sweep2);
-        params.add(sweep3);
-        params.add(sweep4);
+		List<ParameterSweep> params = new LinkedList<>();
+		params.add(sweep1);
+		params.add(sweep2);
+		params.add(sweep3);
+		params.add(sweep4);
 
-        CombinedParameterSweep paramSpace = new DefaultCombinedParameterSweep(params,
-                1);
+		CombinedParameterSweep paramSpace = new DefaultCombinedParameterSweep(
+				params, 1);
 
-        assertEquals(sweep1.size() * sweep2.size() * sweep3.size(),
-                paramSpace.size());
+		assertEquals(sweep1.size() * sweep2.size() * sweep3.size(),
+				paramSpace.size());
 
-        for (Configuration config : paramSpace) {
-            System.out.println("[" + config.getInt("p1") + ","
-                    + config.getInt("p2") + "," + config.getInt("p3") + "]");
-        }
+		for (Configuration config : paramSpace) {
+			System.out.println("[" + config.getInt("p1") + ","
+					+ config.getInt("p2") + "," + config.getInt("p3") + "]");
+		}
 
+	}
 
-    }
+	@Test
+	public void testMultipleRuns() {
+		SingleValueSweep<Integer> sweep = new SingleValueSweep<>("p1", 1);
+		IntegerSequenceSweep sweepSeq = new IntegerSequenceSweep("p2", 0, -1,
+				-1);
 
-    @Test
-    public void testMultipleRuns() {
-        SingleValueSweep<Integer> sweep = new SingleValueSweep<>("p1", 1);
-        IntegerSequenceSweep sweepSeq = new IntegerSequenceSweep("p2", 0, -1,
-                -1);
+		List<ParameterSweep> params = new LinkedList<>();
+		params.add(sweep);
+		params.add(sweepSeq);
 
-        List<ParameterSweep> params = new LinkedList<>();
-        params.add(sweep);
-        params.add(sweepSeq);
+		DefaultCombinedParameterSweep paramSpace = new DefaultCombinedParameterSweep(
+				params, 5);
 
-        DefaultCombinedParameterSweep paramSpace = new DefaultCombinedParameterSweep(params,
-                4);
+		for (Configuration config : paramSpace) {
+			System.out.println("RUN: "
+					+ config.getInt(DefaultCombinedParameterSweep.RUN_PARAM));
+			System.out.println("[" + config.getInt("p1") + ","
+					+ config.getInt("p2") + "]");
+		}
 
-        for (Configuration config : paramSpace) {
-            System.out.println("[" + config.getInt("p1") + ","
-                    + config.getInt("p2") + "]");
-        }
+		assertEquals((sweep.size() * sweepSeq.size()) * 5, paramSpace.size());
 
-        assertEquals((sweep.size() * sweepSeq.size()) * 4, paramSpace.size());
-
-    }
+	}
 }
