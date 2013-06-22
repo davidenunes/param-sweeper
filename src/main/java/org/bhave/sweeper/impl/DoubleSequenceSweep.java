e70b878ba6a845265146b4adf598abb8660c264f
Davide Nunes <davide@davide-U30Jc.(none)>
2013-06-22 23:46:58 +0100

    minor code tweeks

--- src/main/java/org/bhave/sweeper/impl/DoubleSequenceSweep.java
+++ src/main/java/org/bhave/sweeper/impl/DoubleSequenceSweep.java
@@ -1,25 +1,24 @@
 /**
- * Copyright 2013 Davide Nunes
- * Authors : Davide Nunes <davex.pt@gmail.com>
- * Website : http://davidenunes.com 
- * 
+ * Copyright 2013 Davide Nunes Authors : Davide Nunes <davex.pt@gmail.com>
+ * Website : http://davidenunes.com
+ *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * This file is part of the b-have sweeper library.
- * 
- * The b-have sweeper library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *
+ * The b-have sweeper library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
  * The b-have sweeper library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with the b-have network library.  
- * If not, see <http://www.gnu.org/licenses/gpl.html>.
+ * You should have received a copy of the GNU General Public License along with
+ * the b-have network library. If not, see
+ * <http://www.gnu.org/licenses/gpl.html>.
  */
 package org.bhave.sweeper.impl;
 
@@ -39,211 +38,226 @@ import org.bhave.sweeper.SequenceSweep;
  * Implements the {@link SequenceSweep} Interface to provide a simple way to
  * create sequences of Double values.
  * </p>
- * 
+ *
  * <p>
  * These Objects can also be created using the
  * {@link ParameterSweepUtil#createSweep(java.lang.String, double, double, double) }
  * .
  * </p>
- * 
+ *
  * @see ParameterSweepUtil
- * 
+ *
  * @author Davide Nunes
  */
 public class DoubleSequenceSweep implements SequenceSweep<Double> {
 
-	private final BigDecimal from;
-	private final BigDecimal to;
-	private final BigDecimal step;
-	private final String param;
+    private final BigDecimal from;
+    private final BigDecimal to;
+    private final BigDecimal step;
+    private final String param;
 
-	/**
-	 * Constructor for this immutable Integer Sequence Sweep
-	 * 
-	 * @param from
-	 *            the first item in the sequence
-	 * @param to
-	 *            the last item in the sequence
-	 * @param step
-	 *            the step to update the sequence
-	 */
-	public DoubleSequenceSweep(String param, double from, double to, double step)
-			throws IllegalArgumentException {
-		this.from = new BigDecimal(from);
-		this.to = new BigDecimal(to);
-		this.step = new BigDecimal(step);
-		this.param = param;
+    /**
+     * Constructor for this immutable Integer Sequence Sweep
+     *
+     * @param from the first item in the sequence
+     * @param to the last item in the sequence
+     * @param step the step to update the sequence
+     */
+    public DoubleSequenceSweep(String param, double from, double to, double step)
+            throws IllegalArgumentException {
+        this.from = BigDecimal.valueOf(from);
+        this.to = BigDecimal.valueOf(to);
+        this.step = BigDecimal.valueOf(step);
+        this.param = param;
 
-		if (step == 0) {
-			throw new IllegalArgumentException("step cannot be 0");
-		}
-		if ((to > from && step < 0) || to < from && step > 0) {
-			throw new IllegalArgumentException(
-					"step must be positive if to > from and negative if to < from");
-		}
 
-	}
 
-	@Override
-	public String getParameterName() {
-		return param;
-	}
+        if (step == 0) {
+            throw new IllegalArgumentException("step cannot be 0");
+        }
+        if ((to > from && step < 0) || to < from && step > 0) {
+            throw new IllegalArgumentException(
+                    "step must be positive if to > from and negative if to < from");
+        }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public Iterator<Configuration> iterator() {
-		TransformIterator it = new TransformIterator(
-				new DoubleSequenceIterator(),
-				new ConfigurationTranformer(param));
+    }
 
-		return it;
-	}
+    @Override
+    public String getParameterName() {
+        return param;
+    }
 
-	@Override
-	public Double from() {
-		return from.doubleValue();
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public Iterator<Configuration> iterator() {
+        TransformIterator it = new TransformIterator(
+                new DoubleSequenceIterator(),
+                new ConfigurationTranformer(param));
 
-	@Override
-	public Double to() {
-		return to.doubleValue();
-	}
+        return it;
+    }
 
-	@Override
-	public Double step() {
-		return step.doubleValue();
-	}
+    @Override
+    public Double from() {
+        return from.doubleValue();
+    }
 
-	/**
-	 * An iterator that generates a sequence of Double objects on demand. This
-	 * means that the actual sequence is not generated until the next method is
-	 * called.
-	 */
-	private class DoubleSequenceIterator implements Iterator<Double> {
+    @Override
+    public Double to() {
+        return to.doubleValue();
+    }
 
-		private BigDecimal currentValue;
+    @Override
+    public Double step() {
+        return step.doubleValue();
+    }
 
-		public DoubleSequenceIterator() {
-			this.currentValue = new BigDecimal(from.doubleValue());
-		}
+    /**
+     * An iterator that generates a sequence of Double objects on demand. This
+     * means that the actual sequence is not generated until the next method is
+     * called.
+     */
+    private class DoubleSequenceIterator implements Iterator<Double> {
 
-		boolean canStep = true;
+        private BigDecimal currentValue;
+        private Iterator<Configuration> stepper;
 
-		@Override
-		public boolean hasNext() {
-			if (!canStep) {
-				return false;
-			}
+        public DoubleSequenceIterator() {
+            this.currentValue = BigDecimal.valueOf(from.doubleValue());
+            //to minimise rounding errors this IntegerSequence does the stepping instead
+            //the next double value is computed by from + (step * stepper.next) 
+            IntegerSequenceSweep integerSeq =
+                    new IntegerSequenceSweep(param, 0, //From
+                    (int) Math.rint((to.doubleValue() - from.doubleValue()) / step.doubleValue()), //to
+                    1); //step
+            stepper = integerSeq.iterator();
+        }
+        boolean canStep = true;
 
-			return step.signum() > 0 ? currentValue.compareTo(to) <= 0
-					: currentValue.compareTo(to) >= 0;
-		}
+        @Override
+        public boolean hasNext() {
+            if (!canStep) {
+                return false;
+            }
 
-		@Override
-		public Double next() {
-			double result = currentValue.round(MathContext.DECIMAL32)
-					.doubleValue();
+            return stepper.hasNext();
+        }
 
-			if (step.signum() == 0) {
-				canStep = false;
-			}
-			currentValue = currentValue.add(step);
-			currentValue = currentValue.round(MathContext.DECIMAL32);
+        @Override
+        public Double next() {
+            double result;
 
-			return result;
-		}
+            if (step.signum() == 0) {
+                canStep = false;
+            }
 
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException(
-					"You cannot remove items from a sweep");
-		}
-	}
+            result = step.multiply(new BigDecimal(stepper.next().getInt(param))).add(from).doubleValue();
+            currentValue = BigDecimal.valueOf(result);
 
-	/**
-	 * Constructs the whole Sequence and returns it as a list
-	 */
-	@Override
-	public List<Double> getValues() {
-		Iterator<Double> it = new DoubleSequenceIterator();
-		List<Double> values = new ArrayList<>();
-		while (it.hasNext()) {
-			values.add(it.next());
-		}
+            return result;
+        }
 
-		return values;
-	}
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException(
+                    "You cannot remove items from a sweep");
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "[from:" + from + ", to:" + to + ", step:" + step + "]";
+    /**
+     * Constructs the whole Sequence and returns it as a list
+     */
+    @Override
+    public List<Double> getValues() {
+        Iterator<Double> it = new DoubleSequenceIterator();
+        List<Double> values = new ArrayList<>();
+        while (it.hasNext()) {
+            values.add(it.next());
+        }
 
-	}
+        return values;
+    }
 
-	/**
-	 * Returns an iterator over the possible Double values generator by this
-	 * Parameter Sweep
-	 * 
-	 * @return an Iterator over Double objects
-	 */
-	@Override
-	public Iterator<Double> valueIterator() {
-		return new DoubleSequenceIterator();
-	}
+    @Override
+    public String toString() {
+        return "[from:" + from + ", to:" + to + ", step:" + step + "]";
 
-	@Override
-	public int size() {
-		BigDecimal diff = to.subtract(from).abs();
+    }
 
-		int size = diff.divide(step.abs(), RoundingMode.FLOOR).intValue();
+    /**
+     * Returns an iterator over the possible Double values generator by this
+     * Parameter Sweep
+     *
+     * @return an Iterator over Double objects
+     */
+    @Override
+    public Iterator<Double> valueIterator() {
+        return new DoubleSequenceIterator();
+    }
 
-		return (!(to.compareTo(BigDecimal.ZERO) < 0
-				&& from.compareTo(BigDecimal.ZERO) > 0 || to
-				.compareTo(BigDecimal.ZERO) > 0
-				&& from.compareTo(BigDecimal.ZERO) < 0)) ? (size + 1) : size;
-	}
+    @Override
+    public int size() {
+        BigDecimal diff = to.subtract(from).abs();
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((from == null) ? 0 : from.hashCode());
-		result = prime * result + ((param == null) ? 0 : param.hashCode());
-		result = prime * result + ((step == null) ? 0 : step.hashCode());
-		result = prime * result + ((to == null) ? 0 : to.hashCode());
-		return result;
-	}
+        int size = diff.divide(step.abs(), RoundingMode.FLOOR).intValue();
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DoubleSequenceSweep other = (DoubleSequenceSweep) obj;
-		if (from == null) {
-			if (other.from != null)
-				return false;
-		} else if (!from.equals(other.from))
-			return false;
-		if (param == null) {
-			if (other.param != null)
-				return false;
-		} else if (!param.equals(other.param))
-			return false;
-		if (step == null) {
-			if (other.step != null)
-				return false;
-		} else if (!step.equals(other.step))
-			return false;
-		if (to == null) {
-			if (other.to != null)
-				return false;
-		} else if (!to.equals(other.to))
-			return false;
-		return true;
-	}
+        return (!(to.compareTo(BigDecimal.ZERO) < 0
+                && from.compareTo(BigDecimal.ZERO) > 0 || to
+                .compareTo(BigDecimal.ZERO) > 0
+                && from.compareTo(BigDecimal.ZERO) < 0)) ? (size + 1) : size;
+    }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((from == null) ? 0 : from.hashCode());
+        result = prime * result + ((param == null) ? 0 : param.hashCode());
+        result = prime * result + ((step == null) ? 0 : step.hashCode());
+        result = prime * result + ((to == null) ? 0 : to.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        DoubleSequenceSweep other = (DoubleSequenceSweep) obj;
+        if (from == null) {
+            if (other.from != null) {
+                return false;
+            }
+        } else if (!from.equals(other.from)) {
+            return false;
+        }
+        if (param == null) {
+            if (other.param != null) {
+                return false;
+            }
+        } else if (!param.equals(other.param)) {
+            return false;
+        }
+        if (step == null) {
+            if (other.step != null) {
+                return false;
+            }
+        } else if (!step.equals(other.step)) {
+            return false;
+        }
+        if (to == null) {
+            if (other.to != null) {
+                return false;
+            }
+        } else if (!to.equals(other.to)) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/test/java/org/bhave/sweeper/DoubleSequenceSweepTest.java
+++ src/test/java/org/bhave/sweeper/DoubleSequenceSweepTest.java
@@ -1,28 +1,28 @@
 /**
- * Copyright 2013 Davide Nunes
- * Authors : Davide Nunes <davex.pt@gmail.com>
- * Website : http://davidenunes.com 
- * 
+ * Copyright 2013 Davide Nunes Authors : Davide Nunes <davex.pt@gmail.com>
+ * Website : http://davidenunes.com
+ *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * This file is part of the b-have sweeper library.
- * 
- * The b-have sweeper library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *
+ * The b-have sweeper library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
  * The b-have sweeper library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with the b-have network library.  
- * If not, see <http://www.gnu.org/licenses/gpl.html>.
+ * You should have received a copy of the GNU General Public License along with
+ * the b-have network library. If not, see
+ * <http://www.gnu.org/licenses/gpl.html>.
  */
 package org.bhave.sweeper;
 
+import java.math.BigDecimal;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -34,50 +34,70 @@ import org.junit.Test;
 
 public class DoubleSequenceSweepTest {
 
-	@Test
-	public void test() {
-		DoubleSequenceSweep seq = new DoubleSequenceSweep("p1", 0, 2, 1.2);
+    @Test
+    public void test() {
+        DoubleSequenceSweep seq = new DoubleSequenceSweep("p1", 0, 2, 1.2);
 
-		assertEquals(2, seq.size());
+        assertEquals(2, seq.size());
 
-		DoubleSequenceSweep seq2 = new DoubleSequenceSweep("p1", 0, 2, 0.7);
+        DoubleSequenceSweep seq2 = new DoubleSequenceSweep("p1", 0, 2, 0.7);
 
-		assertEquals(3, seq2.size());
+        assertEquals(3, seq2.size());
 
-		DoubleSequenceSweep seq3 = new DoubleSequenceSweep("p1", 0, 2, 0.25);
+        DoubleSequenceSweep seq3 = new DoubleSequenceSweep("p1", 0, 2, 0.25);
 
-		assertEquals(9, seq3.size());
+        assertEquals(9, seq3.size());
 
-	}
+    }
 
-	@Test
-	public void testPrecision() {
-		DoubleSequenceSweep seq = new DoubleSequenceSweep("p1", 0, 1, 0.05);
+    @Test
+    public void testPrecision() {
 
-		double[] expectedValues = new double[] { 0, 0.05, 0.1, 0.15, 0.2, 0.25,
-				0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8,
-				0.85, 0.9, 0.95, 1 };
+        DoubleSequenceSweep seq = new DoubleSequenceSweep("p1", 0, 1, 0.05);
 
-		Iterator<Configuration> it = seq.iterator();
-		for (double expected : expectedValues) {
-			Configuration config = it.next();
-			double generated = config.getDouble("p1");
-			assertTrue(generated == expected);
-		}
-	}
+        double[] expectedValues = new double[]{0, 0.05, 0.1, 0.15, 0.2, 0.25,
+            0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8,
+            0.85, 0.9, 0.95, 1};
 
-	@Test
-	public void testAdditionalDecimalPlaces() {
-		DoubleSequenceSweep seq = new DoubleSequenceSweep("p1", 0, 1, 0.05);
+        Iterator<Configuration> it = seq.iterator();
+        for (double expected : expectedValues) {
+            Configuration config = it.next();
+            double generated = config.getDouble("p1");
+            assertTrue(generated == expected);
+        }
 
-		for (Configuration config : seq) {
-			System.out.println(config.getDouble("p1"));
-		}
-	}
-	
-	@Test
-	public void testSweepSize(){
-		DoubleSequenceSweep seq = new DoubleSequenceSweep("p1", 0, 0.05, 0.05);
-		assertEquals(2, seq.size());
-	}
+
+    }
+    @Test
+    public void testReverse() {
+
+        DoubleSequenceSweep seq = new DoubleSequenceSweep("p1", 1, 0.90, -0.05);
+
+        double[] expectedValues = new double[]{1, 0.95, 0.90};
+
+        Iterator<Configuration> it = seq.iterator();
+        for (double expected : expectedValues) {
+            Configuration config = it.next();
+            double generated = config.getDouble("p1");
+            System.out.println(generated);
+            assertTrue(generated == expected);
+        }
+
+
+    }
+
+    @Test
+    public void testAdditionalDecimalPlaces() {
+        DoubleSequenceSweep seq = new DoubleSequenceSweep("p1", 0, 1, 0.05);
+
+        for (Configuration config : seq) {
+            System.out.println(config.getDouble("p1"));
+        }
+    }
+
+    @Test
+    public void testSweepSize() {
+        DoubleSequenceSweep seq = new DoubleSequenceSweep("p1", 0, 0.05, 0.05);
+        assertEquals(2, seq.size());
+    }
 }
